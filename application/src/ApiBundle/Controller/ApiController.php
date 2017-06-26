f87ead73c90c5d00c87a22fe3fc34dd7049d8381
Alin Obedea <alinobedea@gmail.com>
2017-06-26 17:30:04 +0300

    Update the ApiController

--- application/src/ApiBundle/Controller/ApiController.php
+++ application/src/ApiBundle/Controller/ApiController.php
@@ -11,7 +11,7 @@ class ApiController extends Controller
     /**
      * @Route("/api/posts")
      */
-    public function indexAction(int $id)
+    public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
         $posts = $em->getRepository(Post::class)->findBy(['author' => $this->getUser()], ['publishedAt' => 'DESC'], ['limit' => '10']);
@@ -21,15 +21,31 @@ class ApiController extends Controller
 
     /**
      *
-     * @Route("/page/{page}", requirements={"page": "[1-9]\d*"})
+     * @Route("/posts/{page}", requirements={"page": "[1-9]\d*"})
      * @Method("GET")
      * @Cache(smaxage="10")
-     *
+     * @param $page
+     * @return
      */
     public function getAction($page)
     {
         $em = $this->getDoctrine()->getManager();
         $posts = $em->getRepository(Post::class)->findLatest($page);
+
+        return $posts;
+    }
+
+    /**
+     *
+     * @Route("/post/{slug}", requirements={"page": "[1-9]\d*"})
+     * @Method("GET")
+     * @param $slug
+     * @return object
+     */
+    public function showAction($slug)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $posts = $em->getRepository(Post::class)->findOneBy($slug);
 
         return $posts;
     }
