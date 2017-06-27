129417eb29f1f4171d936a282968d1a5bc417291
Alin Obedea <alinobedea@gmail.com>
2017-06-27 12:09:49 +0300

    Add the new endpoint for comments

--- application/src/ApiBundle/Controller/ApiController.php
+++ application/src/ApiBundle/Controller/ApiController.php
@@ -2,6 +2,7 @@
 
 namespace ApiBundle\Controller;
 
+use AppBundle\Entity\Comment;
 use AppBundle\Entity\Post;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -15,16 +16,16 @@ class ApiController extends FOSRestController
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
-        $posts = $em->getRepository(Post::class)->findBy(['author' => $this->getUser()], ['publishedAt' => 'DESC'], ['limit' => 10]);
+        $posts = $em->getRepository(Post::class)->findBy([], [], Post::NUM_ITEMS);
 
         return $this->render('api/blog/index.html.twig', ['posts' => $posts]);
     }
 
     /**
      *
-     * @Route("/api/posts/{page}", requirements={"page": "[1-9]\d*"})
+     * @Route("/api/posts/{page}", requirements={"page": "[1-9]\d*"}, defaults={"page" = 1})
      * @param $page
-     * @return
+     * @return object
      */
     public function getAction($page)
     {
@@ -43,8 +44,22 @@ class ApiController extends FOSRestController
     public function showAction($slug)
     {
         $em = $this->getDoctrine()->getManager();
-        $posts = $em->getRepository(Post::class)->findOneBy($slug);
+        $posts = $em->getRepository(Post::class)->findBy(['slug' => $slug], [], 1);
 
         return $this->render('api/blog/index.html.twig', ['posts' => $posts]);
+    }
+
+    /**
+     *
+     * @Route("/api/posts/comment/{slug}")
+     * @param $slug
+     * @return object
+     */
+    public function showCommentAction($slug)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $post = $em->getRepository(Post::class)->findOneBy(['slug' => $slug]);
+
+        return $this->render('api/blog/comment.html.twig', ['comments' => $post->getComments()]);
     }
 }
